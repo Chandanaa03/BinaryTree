aab130a67d9465aa6d929ae1a7b128b4c6930d1a
chandana03 <chandanareshma01@gmail.com>
2022-03-24 00:36:50 +0530

    hashtable

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Hashtabledemo
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+
+            table.Add(1, "to be");
+            table.Add(2, "not to be");
+            table.Add(3, "or");
+            //table.Add(4, "not");
+
+            foreach(DictionaryEntry ele in table)
+            {
+                Console.WriteLine(ele.Key + "-" + ele.Value);
+            }
+            //Console.WriteLine("---------");
+
+            //Console.WriteLine(table.ContainsKey(3));
+
+            Console.WriteLine("----------");
+            Console.WriteLine(table[1]);
+
+        }
+    }
+}
