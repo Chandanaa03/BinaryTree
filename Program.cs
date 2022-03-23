2056138fb915903a16e4a6cf8d69f71dc0e1d3ab
chandana03 <chandanareshma01@gmail.com>
2022-03-23 19:56:39 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tree Tree = new tree();
+            Tree.Add(56);
+            Tree.Add(30);
+            Tree.Add(70);
+            
+
+            Tree.TraversePreorder(Tree.Root);
+
+        }
+    }
+}
