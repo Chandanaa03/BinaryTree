f9fce5a1fdc6d53561f9a12673718d01601c6890
chandana03 <chandanareshma01@gmail.com>
2022-03-23 21:04:35 +0530

    Node uc2

--- Node.cs
+++ Node.cs
@@ -8,10 +8,38 @@ namespace BinaryTree
 {
     public class Node
     {
-        public Node LeftNode { get; set; }
-        public Node RightNode { get; set; }
-        public int data { get; set; }
+        //public Node LeftNode { get; set; }
+        //public Node RightNode { get; set; }
+        //public int data { get; set; }
+        public int data;
+        public Node left, right;
 
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+    public class BinaryTree
+    {
+        public Node root;
+
+        public virtual int size()
+        {
+            return size(root);
+        }
+        public virtual int size(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return (size(node.left) + 1 + size(node.right));
+            }
+        }
 
     }
+    
 }
